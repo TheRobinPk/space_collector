920f7dd55172ec83d0818a8da91a75fa549842fc
Vincent Poulailleau <vpoulailleau@gmail.com>
2024-01-09 15:00:39 +0100

    add glowing stars

--- space_collector/viewer/space_background.py
+++ space_collector/viewer/space_background.py
@@ -1,5 +1,6 @@
 import random
 from pathlib import Path
+import logging
 
 import arcade
 
@@ -16,20 +17,38 @@ def find_image_files(directory: Path | str) -> list[Path]:
     ]
 
 
+def random_image(path: str) -> arcade.Sprite:
+    sprite_image = random.choice(find_image_files(path))
+    sprite = arcade.Sprite(sprite_image)
+    sprite.position = SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2
+    sprite.width = SCREEN_WIDTH
+    sprite.height = SCREEN_HEIGHT
+    return sprite
+
+
+def alpha(frame: int, period: int, max_value: int) -> int:
+    frame %= period
+    value = abs(frame / period - 0.5) * 2
+    return int(max_value * value)
+
+
 class SpaceBackground:
     def __init__(self):
         self.sprite_list = arcade.SpriteList()
+        self.frame_index = 0
 
     def setup(self) -> None:
         self.sprite_list = arcade.SpriteList()
-        background_image = random.choice(
-            find_image_files("space_collector/viewer/images/backgrounds")
+        self.sprite_list.append(
+            random_image("space_collector/viewer/images/backgrounds")
         )
-        background = arcade.Sprite(background_image)
-        background.position = SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2
-        background.width = SCREEN_WIDTH
-        background.height = SCREEN_HEIGHT
-        self.sprite_list.append(background)
+        self.starfield1 = random_image("space_collector/viewer/images/starfields")
+        self.sprite_list.append(self.starfield1)
+        self.starfield2 = random_image("space_collector/viewer/images/starfields")
+        self.sprite_list.append(self.starfield2)
 
     def draw(self):
+        self.frame_index += 1
+        self.starfield1.alpha = alpha(self.frame_index, 1002, 30)
+        self.starfield2.alpha = alpha(self.frame_index, 3107, 40)
         self.sprite_list.draw()
