df6ceee69400ba246bf4994d9eaf3918be83df59
Vincent Poulailleau <vpoulailleau@gmail.com>
2024-01-11 16:43:38 +0100

    fix start angle

--- space_collector/game/player.py
+++ space_collector/game/player.py
@@ -5,13 +5,12 @@ class Player:
     def __init__(self, name: str) -> None:
         self.name = name
         self.blocked = False
-        self.orientation = "S"
         self.spaceships: list[Spaceship] = []
         # TODO remove dummy init
         self.spaceships = [
-            Collector(1, 100, 0, 270),
-            Attacker(2, 200, 0, 270),
-            Explorer(3, 300, 0, 270),
+            Collector(1, 100, 0, 90),
+            Attacker(2, 200, 0, 90),
+            Explorer(3, 300, 0, 90),
         ]
 
     def state(self) -> dict:
