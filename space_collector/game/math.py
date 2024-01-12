0c1631799b9f397c3c05c401127f52b0c3b26d47
Vincent Poulailleau <vpoulailleau@gmail.com>
2024-01-12 11:19:20 +0100

    begin math

---
+++ space_collector/game/math.py
@@ -0,0 +1,49 @@
+from __future__ import annotations
+
+
+class Vector:
+    def __init__(self, data: list) -> None:
+        self.data = data
+
+    def __repr__(self) -> str:
+        return "Vector(" + ", ".join(str(item) for item in self.data) + ")"
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+    def __getitem__(self, index):
+        return self.data[index]
+
+    @property
+    def x(self):
+        return self.data[0]
+
+    @property
+    def y(self):
+        return self.data[1]
+
+    @property
+    def z(self):
+        return self.data[2]
+
+
+class Matrix:
+    def __init__(self, data: list) -> None:
+        """First dimension in data is rows: [[a,b],[c,d]] => [a,b] is first row."""
+        self.data = data
+
+    def __getitem__(self, indices_tuple: tuple[int]):
+        data = self.data
+        indices = list(indices_tuple)
+        while indices:
+            data = data[indices.pop(0)]
+        return data
+
+    def __matmul__(self, other: Vector | Matrix) -> Vector | Matrix:
+        assert isinstance(other, Vector)  # Matrix @ Matrix not implemented yet
+        result = Vector([])
+        for index in range(len(other)):
+            result.data.append(
+                sum(mat * vec for mat, vec in zip(self.data[index], other))
+            )
+        return result
--- space_collector/game/player.py
+++ space_collector/game/player.py
@@ -1,6 +1,9 @@
+import logging
+
 from space_collector.game.spaceship import Spaceship, Collector, Attacker, Explorer
 from space_collector.game.constants import MAP_DIMENSION
 from space_collector.game.planet import Planet
+from space_collector.game.math import Matrix, Vector
 
 
 class Player:
@@ -17,41 +20,39 @@ class Player:
         if team == 0:
             self.base_position = (MAP_DIMENSION // 2, 0)
             angle = 90
-            dx = 1
-            dy = 0
+            matrix = Matrix([[1, 0], [0, 1]])
         elif team == 1:
             self.base_position = (0, MAP_DIMENSION // 2)
             angle = 0
-            dx = 0
-            dy = -1
+            matrix = Matrix([[0, 1], [-1, 0]])
         elif team == 2:
             self.base_position = (MAP_DIMENSION // 2, MAP_DIMENSION)
             angle = 270
-            dx = -1
-            dy = 0
+            matrix = Matrix([[-1, 0], [0, -1]])
         else:
             self.base_position = (MAP_DIMENSION, MAP_DIMENSION // 2)
             angle = 180
-            dx = 0
-            dy = 1
+            matrix = Matrix([[0, -1], [1, 0]])
 
         base_x, base_y = self.base_position
+        origin_x_unit = Vector([1, 0])
+        x_unit = matrix @ origin_x_unit
         self.spaceships = [
             Attacker(1, base_x, base_y, angle),
-            Attacker(2, base_x + 1500 * dx, base_y + 1500 * dy, angle),
-            Attacker(3, base_x - 1500 * dx, base_y - 1500 * dy, angle),
-            Attacker(4, base_x + 3000 * dx, base_y + 3000 * dy, angle),
-            Attacker(5, base_x - 3000 * dx, base_y - 3000 * dy, angle),
-            Explorer(6, base_x + 4500 * dx, base_y + 4500 * dy, angle),
-            Explorer(7, base_x - 4500 * dx, base_y - 4500 * dy, angle),
-            Collector(8, base_x + 6000 * dx, base_y + 6000 * dy, angle),
-            Collector(9, base_x - 6000 * dx, base_y - 6000 * dy, angle),
+            Attacker(2, base_x + 1500 * x_unit.x, base_y + 1500 * x_unit.y, angle),
+            Attacker(3, base_x - 1500 * x_unit.x, base_y - 1500 * x_unit.y, angle),
+            Attacker(4, base_x + 3000 * x_unit.x, base_y + 3000 * x_unit.y, angle),
+            Attacker(5, base_x - 3000 * x_unit.x, base_y - 3000 * x_unit.y, angle),
+            Explorer(6, base_x + 4500 * x_unit.x, base_y + 4500 * x_unit.y, angle),
+            Explorer(7, base_x - 4500 * x_unit.x, base_y - 4500 * x_unit.y, angle),
+            Collector(8, base_x + 6000 * x_unit.x, base_y + 6000 * x_unit.y, angle),
+            Collector(9, base_x - 6000 * x_unit.x, base_y - 6000 * x_unit.y, angle),
         ]
         for planet_x, planet_y in planet_positions:
             self.planets.append(
                 Planet(
-                    base_x + planet_x * dx,  # TODO rotation
-                    base_y + planet_y * dy,
+                    base_x + planet_x * x_unit.x,  # TODO rotation
+                    base_y + planet_y * x_unit.y,
                 )
             )
 
