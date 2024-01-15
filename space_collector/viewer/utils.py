f7df8a2898af67f1bba4206e44a2d2a649b058d6
Vincent Poulailleau <vpoulailleau@gmail.com>
2024-01-15 11:37:16 +0100

    offset hue from proheminent hue

--- space_collector/viewer/utils.py
+++ space_collector/viewer/utils.py
@@ -1,5 +1,7 @@
+import logging
 import random
 import uuid
+from collections import Counter
 from pathlib import Path
 
 import arcade
@@ -45,12 +47,38 @@ def random_sprite(path: str) -> arcade.Sprite:
     return sprite
 
 
+def hue(image: Image) -> int:
+    """Get most common hue in important pixels (saturated, not transparent…)
+
+    Args:
+        image: the image to analyze
+
+    Returns:
+        the most common hue (rounded to tens)
+    """
+    alphas = image.getdata(band=3)
+    hsv_image = image.convert("HSV")
+    hues = hsv_image.getdata(band=0)
+    saturations = hsv_image.getdata(band=1)
+    values = hsv_image.getdata(band=2)
+    important_hues = [
+        hue // 10 * 10
+        for hue, saturation, value, alpha in zip(hues, saturations, values, alphas)
+        if saturation > 50 and value > 50 and alpha != 0
+    ]
+    counter = Counter(important_hues)
+    return counter.most_common(1)[0][0]
+
+
 def hue_changed_texture(image_path: str, target_hue: int) -> arcade.Texture:
+    logging.info("Managing %s", image_path)
     original_image = Image.open(image_path)
+    original_hue = hue(original_image)
+    offset_hue = 256 + target_hue - original_hue
     alpha_channel = original_image.split()[-1]
     hsv_image = original_image.convert("HSV")
     h, s, v = hsv_image.split()
-    hue_data = h.point(lambda i: (i + target_hue) % 256)
+    hue_data = h.point(lambda i: (i + offset_hue) % 256)
     adjusted_hue_image = Image.merge("HSV", (hue_data, s, v))
     adjusted_hue_image_rgb = adjusted_hue_image.convert("RGB")
     final_image = Image.merge("RGBA", (*adjusted_hue_image_rgb.split(), alpha_channel))
