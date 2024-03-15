fc9bfdcfa39255f2e58576da03635d7a09f1d26e
Vincent Poulailleau <vpoulailleau@gmail.com>
2024-03-15 12:07:10 +0100

    continue small window

--- space_collector/viewer/constants.py
+++ space_collector/viewer/constants.py
@@ -4,6 +4,7 @@ import sys
 small_window = False
 
 
+# what an ugly hack… TODO
 def __getattr__(name: str):
     if name == "__path__":
         return __name__
@@ -13,6 +14,20 @@ def __getattr__(name: str):
         return 600 if small_window else 1000
     if name == "SCORE_WIDTH":
         return 300 if small_window else 500
+    if name == "MAP_MARGIN":
+        return 50 if small_window else 170
+    if name == "MAP_MIN_X":
+        return (
+            sys.modules[__name__].SCORE_WIDTH + sys.modules[__name__].MAP_MARGIN * 1.2
+        )
+    if name == "MAP_MAX_X":
+        return int(
+            sys.modules[__name__].SCREEN_WIDTH - sys.modules[__name__].MAP_MARGIN * 1.2
+        )
+    if name == "MAP_MIN_Y":
+        return sys.modules[__name__].MAP_MARGIN
+    if name == "MAP_MAX_Y":
+        return sys.modules[__name__].SCREEN_HEIGHT - sys.modules[__name__].MAP_MARGIN
     return globals()[name]
 
 
@@ -27,9 +42,3 @@ TEAM_COLORS = {
     team: tuple(int(c * 255) for c in colorsys.hsv_to_rgb(hue / 360, 1, 1))
     for team, hue in TEAM_HUES.items()
 }
-
-MAP_MARGIN = 170
-MAP_MIN_X = int(sys.modules[__name__].SCORE_WIDTH + MAP_MARGIN * 1.2)
-MAP_MAX_X = int(sys.modules[__name__].SCREEN_WIDTH - MAP_MARGIN * 1.2)
-MAP_MIN_Y = MAP_MARGIN
-MAP_MAX_Y = sys.modules[__name__].SCREEN_HEIGHT - MAP_MARGIN
