c9e768bbd3abbc47f47901cf75c1629d89c1ad2d
Vincent Poulailleau <vpoulailleau@gmail.com>
2024-03-22 15:25:47 +0100

    add profiling

--- competition.sh
+++ competition.sh
@@ -5,8 +5,10 @@ python  -m space_collector.killall.py
 find . -name "*.log" -exec rm \{} \;
 
 python -m space_collector.game.server -p $PORT &
-python -m space_collector.viewer -p $PORT &
-python -m space_collector.viewer -p $PORT --small-window &
+# python -m space_collector.viewer -p $PORT &
+# https://www.speedscope.app/
+py-spy record -o profile.data --format speedscope -- python -m space_collector.viewer -p $PORT &
+# python -m space_collector.viewer -p $PORT --small-window &
 sleep 2
 # python -m space_collector.serial2tcp -p $PORT &
 python /home/vincent/Documents/programmation/space_collector_player/sample_player_client.py -p $PORT &
