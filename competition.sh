6b50d053b7ddbf7a6059a0796134fcb2b31ea528
Vincent Poulailleau <vpoulailleau@gmail.com>
2025-02-25 15:18:53 +0100

    better competition example

--- competition.sh
+++ competition.sh
@@ -8,13 +8,16 @@ python -m space_collector.game.server -p $PORT &
 python -m space_collector.viewer -p $PORT &
 # https://www.speedscope.app/
 # py-spy record -o profile.data --format speedscope -- python -m space_collector.viewer -p $PORT &
-# python -m space_collector.viewer -p $PORT --small-window &
+#python -m space_collector.viewer -p $PORT --small-window &
 sleep 2
 # python -m space_collector.serial2tcp -p $PORT &
-python /home/vincent/Documents/programmation/space_collector_player/sample_player_client.py -p $PORT &
-python /home/vincent/Documents/programmation/space_collector_player/sample_player_client.py -p $PORT &
-python /home/vincent/Documents/programmation/space_collector_player/sample_player_client.py -p $PORT &
-python /home/vincent/Documents/programmation/space_collector_player/sample_player_client.py -p $PORT &
+python /home/vincent/Documents/programmation/space_collector_player/sample_player_client.py -p $PORT -u "STAR WARS" --winner &
+sleep 1
+python /home/vincent/Documents/programmation/space_collector_player/sample_player_client.py -p $PORT -u "STAR TREK" --rotation-fire &
+sleep 1
+python /home/vincent/Documents/programmation/space_collector_player/sample_player_client.py -p $PORT -u DUNE &
+sleep 1
+python /home/vincent/Documents/programmation/space_collector_player/sample_player_client.py -p $PORT -u "INTERSTELLAR" &
 
-sleep 330
+sleep 700
 python -m space_collector.killall
